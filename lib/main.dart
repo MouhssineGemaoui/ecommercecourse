56c627c0aa60ae4bb757b620a290e465cc513554
MouhssineGemaoui <117443378+MouhssineGemaoui@users.noreply.github.com>
2023-09-30 14:50:17 +0200

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,11 @@ import 'package:get/get.dart';
 import 'package:monami/bindings/initialBinding.dart';
 import 'package:monami/core/localization/changelocal.dart';
 import 'package:monami/core/services/services.dart';
+import 'package:monami/firebase_options.dart';
 import 'package:monami/routes.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:zego_uikit_prebuilt_call/zego_uikit_prebuilt_call.dart';
+import 'package:zego_uikit_signaling_plugin/zego_uikit_signaling_plugin.dart';
 import 'core/localization/translation.dart';
 
 late SharedPreferences sharedPref ; 
@@ -14,12 +17,32 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   // await Firebase.initializeApp();
   await InitialiServices();
-  runApp(const MyApp());
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
+  final navigatorKey = GlobalKey<NavigatorState>();
+
+  /// 1.1.2: set navigator key to ZegoUIKitPrebuiltCallInvitationService
+  ZegoUIKitPrebuiltCallInvitationService().setNavigatorKey(navigatorKey);
+  ZegoUIKit().initLog().then((value) {
+    ///  Call the `useSystemCallingUI` method
+    ZegoUIKitPrebuiltCallInvitationService().useSystemCallingUI(
+      [ZegoUIKitSignalingPlugin()],
+    );
+
+    runApp(MyApp(navigatorKey: navigatorKey));
+  });
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  final GlobalKey<NavigatorState> navigatorKey;
+  MyApp({required this.navigatorKey});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     LocalController controller = Get.put(LocalController());
@@ -34,6 +57,7 @@ class MyApp extends StatelessWidget {
       initialBinding:  initialBindings(),
       // routes: routes,
       getPages: routes,
+      navigatorKey: widget.navigatorKey,
     );
   }
 }
